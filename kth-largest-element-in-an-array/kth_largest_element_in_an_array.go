89713dcf54061e28bc5da8850a4f0c0910dc53b3
hiifong <i@hiif.ong>
2023-08-14 16:12:39 +0800

    Update

--- kth-largest-element-in-an-array/kth_largest_element_in_an_array.go
+++ kth-largest-element-in-an-array/kth_largest_element_in_an_array.go
@@ -1,1 +1,33 @@
 package kth_largest_element_in_an_array
+
+func findKthLargest(nums []int, k int) int {
+	QuickSort(nums, 0, len(nums)-1)
+	return nums[len(nums)-k]
+}
+
+func QuickSort(arr []int, L, R int) {
+	if L >= R {
+		return
+	}
+	pivot := arr[L]
+	left, right := L, R
+	for left < right {
+		for left < right && arr[right] >= pivot {
+			right--
+		}
+		if left < right {
+			arr[left] = arr[right]
+		}
+		for left < right && arr[left] <= pivot {
+			left++
+		}
+		if left < right {
+			arr[right] = arr[left]
+		}
+		if left == right {
+			arr[left] = pivot
+		}
+	}
+	QuickSort(arr, L, right-1)
+	QuickSort(arr, right+1, R)
+}
--- kth-largest-element-in-an-array/kth_largest_element_in_an_array_test.go
+++ kth-largest-element-in-an-array/kth_largest_element_in_an_array_test.go
@@ -1,1 +1,14 @@
 package kth_largest_element_in_an_array
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestQuickSort(t *testing.T) {
+	arr := []int{2, 4, 6, 7, 8, 3, 3, 5, 6, 1, 10, 324, 23, 3, 23, 44, 4, 64, 32, 56, 56, 32, 5, 56, 23, 45}
+	QuickSort(arr, 0, len(arr)-1)
+	for i, v := range arr {
+		fmt.Printf("index: %d --> value: %d\n", i, v)
+	}
+}
